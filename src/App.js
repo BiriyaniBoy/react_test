673956bea63846e4a83195740ae5efcad8ea6a05
biriyaniboy <atlas.one.no1@gmail.com>
2023-12-21 17:38:24 +0530

    subho

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import'./App.css'
+import Chai from'./Chai';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>subhojit rudra paul</h1>
+      <Chai/>
+     
+
+    </div>
+  );
+}
+
+export default App;
